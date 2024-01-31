8163ae8a1e401f09d7a6af28ee435d91b7504b74
Vladimir Rybalsky <34063175+ALEVOLDON@users.noreply.github.com>
2024-01-31 20:52:37 +0300

    upd app

--- main.js
+++ main.js
@@ -6,26 +6,4 @@ function changeBackgroundColor() {
     body.style.backgroundColor = randomColor;
   }
 
-// Installing a service worker
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('my-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/icon.png',
-                // Add other files you want to cache
-            ]);
-        })
-    );
-});
 
-// Using cached resources
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// Installing a service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/icon.png',
+                // Add other files you want to cache
+            ]);
+        })
+    );
+});
+
+// Using cached resources
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
